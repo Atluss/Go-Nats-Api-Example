8fbeca88cf5dbc40bb6d8f5461c273982a34d491
Shokin Ilya <atlus3@mail.ru>
2019-03-25 22:30:34 +0300

    Add program exit

--- api/api.go
+++ api/api.go
@@ -6,6 +6,9 @@ import (
 	"github.com/Atluss/Go-Nats-Api-Example/lib/config"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -23,9 +26,19 @@ func main() {
 	log.Printf("Nats address: %s", set.Config.Nats.Address[0].Address)
 	log.Printf("Nats address(multi): %s", set.Config.GetNatsAddresses())
 
+	// do something if user close program (close DB, or wait running query)
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		log.Println("Exit program...")
+		os.Exit(1)
+	}()
+
 	// setup nats queue for test request
 	err := apiV1.NewV1Test(set)
 	lib.LogOnError(err, "warning")
 
 	log.Fatal(http.ListenAndServe(":10000", set.Route))
+
 }
